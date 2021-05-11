6945448b6bb0e4f796b6b6db26e0cc5cd01b3f89
Eduard Stepanov <neyasyt84@gmail.com>
2021-05-11 21:21:35 +0700

    Second version

--- HoumWorkOOP.py
+++ HoumWorkOOP.py
@@ -35,6 +35,9 @@ class Student:
         return 'Имя: '+self.name+'\nФамилия: '+self.surname+'\nСредняя оценка за домашние задания: '+str('%.2f' % av_grade_stud)+'\nКурсы в процессе обучения: '+str(', '.join(self.courses_in_progress))+'\nЗавершенные курсы: '+'Введение в программирование'
     def out_avgr(self):
         print(self.av_grade)
+    def __lt__(self, other):
+        print('Сравниваем среднюю оценку студента и лектора')
+        return self.av_grade < other.av_grade
 
 class Mentor:
     def __init__(self, name, surname):
@@ -74,6 +77,9 @@ class Lecturer(Mentor):
                 lect_list.append(val)
         av_grade_lect = sum(lect_list) / len(lect_list)
         return 'Имя: '+self.name+'\nФамилия: '+self.surname+'\nСредняя оценка за лекции: '+str('%.2f' % av_grade_lect)
+    def __lt__(self, other):
+        print('Сравниваем среднюю оценку лектора и студента')
+        return self.av_grade < other.av_grade
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -91,20 +97,24 @@ class Reviewer(Mentor):
     def __str__(self):
         return 'Имя: '+self.name+'\nФамилия: '+self.surname
 
-class Medium(Lecturer, Student):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.name = name
-        self.surname = surname
-        self.av_grade = 0
-    def average_rating(self, av_grade):
-        rating_list = []
-        for lang, grade in self.grades.items():
+def aver_stud_or_lect(dict1, dict2):
+    all_dict = {}
+    our_list = []
+    all_dict = dict1.copy()
+    for lang, grade in dict1.items():
+        for lang1, grade1 in dict2.items():
+            if lang != lang1:
+                all_dict.setdefault(lang1, grade1)
+            else:
+                for val in grade1:
+                    grade.append(val)
+    course = input('Введите название курса: ')
+    for lang, grade in all_dict.items():
+        if lang == course:
             for val in grade:
-                rating_list.append(val)
-        av_grade = sum(rating_list) / len(rating_list)
-    def __lt__(self, other):
-        return self.av_grade > other.av_grade
+                our_list.append(val)
+    av_grade_stud = sum(our_list) / len(our_list)
+    print(f" '\nСредняя оценка курса '{course}' : {av_grade_stud}")
 
 one_student = Student('Roy', 'Eman', 'your_gender')
 one_student.courses_in_progress += ['Python', 'Git', 'Java']
@@ -162,11 +172,11 @@ print('Student')
 print(one_student)
 print(two_student)
 print()
-print('one_student.grades', one_student.grades)
-print('two_student.grades', two_student.grades)
-print('one_teacher.grades', one_teacher.grades)
-print('two_teacher.grades', two_teacher.grades)
-av_student = Medium('Roy', 'Eman')
-av_teacher = Medium('Lo', 'Fen')
-print(av_teacher > av_student)
+# print('one_student.grades', one_student.grades)
+# print('two_student.grades', two_student.grades)
+# print('one_teacher.grades', one_teacher.grades)
+# print('two_teacher.grades', two_teacher.grades)
+print(one_student < one_teacher)
+print()
+aver_stud_or_lect(one_student.grades, two_student.grades)
 
